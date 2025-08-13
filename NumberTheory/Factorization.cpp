0a91a2d70fae94d331745b489ce51507b1af1712
Mohamedkhaled687 <mohamedkhaled47526@gmail.com>
2025-08-14 00:34:33 +0300

    update in NumberTheory

---
+++ NumberTheory/Factorization.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<int> fact(int n)
+{
+    vector<int> result;
+    for (int i = 1; i * i <= n; i++)
+    {
+        if (n % i == 0)
+        {
+            result.push_back(i);
+            if (i != n / i)
+                result.push_back(n / i);
+        }
+    }
+    sort(result.begin(), result.end());
+    return result;
+}
